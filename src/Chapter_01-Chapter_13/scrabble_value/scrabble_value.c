dfd7b353624b4bdbf03a927b6f8004734e13deed
Alexander Grishankov <grishankov.alexander@gmail.com>
2021-09-04 18:09:34 +0300

    Adds function that returns string scrabble value.

--- src/Chapter_01-Chapter_13/scrabble_value/scrabble_value.c
+++ src/Chapter_01-Chapter_13/scrabble_value/scrabble_value.c
@@ -5,32 +5,53 @@
 
 #include <ctype.h>
 #include <stdio.h>
+#include <string.h>
 
 
 #define NUM_ELEMENTS(x) ((int) (sizeof(x) / sizeof((x)[0])))
+#define WORD_SIZE 20
+
+
+// Array containing scrabble points for each letter
+static const char letter_points['Z' - 'A' + 1] = {
+	1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5,
+	1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4,
+	4, 8, 4, 10
+};
+
+
+// Returns number of scrabble points in a word
+static int scrabble_value(const char *word);
+
+
 int main(void)
 {
-	const char letter_points['Z' - 'A' + 1] = {
-		1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5,
-		1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4,
-		4, 8, 4, 10
-	};
-	char c;
-	int value = 0;
+	char word[WORD_SIZE];
 
-	printf("Enter a word (0 to exit): ");
-
-	while((c = toupper(getchar())) != '0') {
-		if (c == '\n') {
-			printf("Scrabble value: %d\n", value);
-			printf("Enter a word (0 to exit): ");
-			value = 0;
-			continue;
-		}
-		value += letter_points[c - 'A'];
+	while (1) {
+		printf("Enter scrabble word: (0 to exit) ");
+		scanf("%s", word);
+		if (strcmp(word, "0") == 0)
+			break;
+		printf("Scrabble value of %s is %d\n",
+			word, scrabble_value(word));
 	}
 
-	printf("Scrabble value: %d\n", value);
-
 	return 0;
+}
+
+
+static int scrabble_value(const char *word)
+{
+	int sum = 0;
+	char c;
+
+	while (*word) {
+		c = toupper(*word);
+		if ('A' <= c && c <= 'Z') 
+			sum += letter_points[c - 'A'];
+		word++;
+	}
+
+	return sum;
 }
