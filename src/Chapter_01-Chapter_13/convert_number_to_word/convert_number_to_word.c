22c6c9b027f453249fdcbcf6bd8703d6a0d9fb97
Alexander Grishankov <grishankov.alexander@gmail.com>
2021-09-04 16:43:58 +0300

    Get rid of switch statements. Use strings.

--- src/Chapter_01-Chapter_13/convert_number_to_word/convert_number_to_word.c
+++ src/Chapter_01-Chapter_13/convert_number_to_word/convert_number_to_word.c
@@ -5,53 +5,43 @@
 
 
 #include <stdio.h>
+#include <string.h>
+
+
+// Array that represents words for tenths
+static const char *tenths[] = {
+	"ten", "twenty", "thirty", "fourty",
+	"fifty", "sixty", "seventy", "eighty",
+	"ninety"
+};
+// Array that represents words for ones
+static const char *ones[] = {
+	"one", "two", "three", "four",
+	"five", "six", "seven", "eight",
+	"nine"
+};
+// Array that represents digits from 11 to 19
+static const char *spec_numbers[] = {
+	"eleven", "twelve", "thirteen", "fourteen",
+	"fifteen", "sixteen", "seventeen", "eighteen",
+	"nineteen"
+};
 
 
 int main(void)
 {
-	int ones, tenths;
+	int fd, sd;
 
-	printf("Enter a dwo-digit number: ");
-	scanf("%1d%1d", &tenths, &ones);
+	printf("Enter 2-digit number: ");
+	if (scanf("%1d%1d", &fd, &sd) != 2)
+		return 1;
 
-	switch (tenths) {
-		case 2: printf("twenty"); break;
-		case 3: printf("thirty"); break;
-		case 4: printf("fourty"); break;
-		case 5: printf("fifty"); break;
-		case 6: printf("sixty"); break;
-		case 7: printf("seventy"); break;
-		case 8: printf("eighty"); break;
-		case 9: printf("ninety"); break;
-	}
+	if (fd == 0 && sd == 0)	printf("zero");
+	else if (fd == 0)	printf("%s", ones[sd - 1]);
+	else if (sd == 0)	printf("%s", tenths[fd - 1]);
+	else if (fd == 1)	printf("%s", spec_numbers[sd - 1]);
+	else			printf("%s-%s", tenths[fd - 1], ones[sd - 1]);
 
-	if (tenths != 1) {
-		if (ones && tenths != 0) printf("-");
-		switch (ones) {
-			case 1: printf("one"); break;
-			case 2: printf("two"); break;
-			case 3: printf("three"); break;
-			case 4: printf("four"); break;
-			case 5: printf("five"); break;
-			case 6: printf("six"); break;
-			case 7: printf("seven"); break;
-			case 8: printf("eight"); break;
-			case 9: printf("nine"); break;
-		}
-	} else {
-		switch (ones) {
-			case 0: printf("ten"); break;
-			case 1: printf("eleven"); break;
-			case 2: printf("twelve"); break;
-			case 3: printf("thirteen"); break;
-			case 4: printf("fourteen"); break;
-			case 5: printf("fifteen"); break;
-			case 6: printf("sixteen"); break;
-			case 7: printf("seventeen"); break;
-			case 8: printf("eighteen"); break;
-			case 9: printf("nineteen"); break;
-		}
-	}
 	printf("\n");
 
 	return 0;
