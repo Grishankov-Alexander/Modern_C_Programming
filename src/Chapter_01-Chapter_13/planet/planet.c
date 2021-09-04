4b8b41396ce224f80b274f93a106207c590435d0
Alexander Grishankov <grishankov.alexander@gmail.com>
2021-09-04 15:07:50 +0300

    Compare planets case-insensitive.

--- src/Chapter_01-Chapter_13/planet/planet.c
+++ src/Chapter_01-Chapter_13/planet/planet.c
@@ -5,6 +5,7 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 
 #define NUM_PLANETS 9
@@ -17,16 +18,34 @@ static char *planets[NUM_PLANETS] = {
 };
 
 
+// Compare case insensitive
+static int mystricmp(const char *s1, const char *s2);
+
+
 int main(int argc, char *argv[])
 {
-	char **p, **q = planets;
+	char **p, **q;
 
-	for (p = argv + 1; *p != NULL; p++) {
+	for (p = argv + 1; p < argv + argc; p++) {
 		for (q = planets; q < planets + NUM_PLANETS; q++)
-			if (strcmp(*p, *q) == 0)
+			if (mystricmp(*p, *q) == 0)
 				printf("%s is a planet number %ld\n",
 					*p, q - planets + 1);
 	}
 
 	return 0;
+}
+
+
+int mystricmp(const char *s1, const char *s2)
+{
+	int diff;
+
+	for ( ; ; s1++, s2++) {
+		diff = tolower(*s1) - tolower(*s2);
+		if (diff != 0 || *s1 == 0)
+			break;
+	}
+
+	return diff;
 }
