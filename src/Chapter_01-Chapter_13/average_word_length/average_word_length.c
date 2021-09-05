1ab6025f65eb0dee07724985b5fb70c20d941cd9
Alexander Grishankov <grishankov.alexander@gmail.com>
2021-09-05 16:12:31 +0300

    Introduce function to comute average word length.

--- src/Chapter_01-Chapter_13/average_word_length/average_word_length.c
+++ src/Chapter_01-Chapter_13/average_word_length/average_word_length.c
@@ -5,35 +5,49 @@
 
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
+
+
+#define MAX_STR_LEN 70
+
+
+// Computes average word length of a sentence
+static double avg_word_len(const char *sentence);
 
 
 int main(void)
 {
-	int letters = 0, words = 0;
-	char ch;
-	bool count = false;
+	char sentence[MAX_STR_LEN];
 
 	printf("Enter a sentence: ");
+	fgets(sentence, sizeof sentence, stdin);
+	*(sentence + strlen(sentence) - 1) = 0;
 
-	while ((ch = getchar()) != '\n') {
-		switch (ch) {
-			case ' ':
-				if (count) {
-					count = false;
-					words++;
-				}
-				break;
-			default:
-				if (!count)
-					count = true;
-				letters++;
-				break;
+	printf("Average word length: %g\n", avg_word_len(sentence));
+
+	return 0;
+}
+
+
+static double avg_word_len(const char *sentence)
+{
+	int num_letters = 0, num_words = 0;
+	bool in_word = false;
+
+	for ( ; *sentence; sentence++) {
+		if (!in_word && isalpha(*sentence)) {
+			in_word = true;
+			num_words++;
 		}
+		else if (in_word && !isalpha(*sentence))
+			in_word = false;
+		if (!in_word)
+			continue;
+		num_letters++;
 	}
-	if (count)
-		count = false, words++;
 
-	printf("Average word length: %.1f\n", (float)letters / words);
-
+	if (num_words)
+		return (double) num_letters / num_words;
 	return 0;
 }
