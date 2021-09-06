3a895e0e0aa3b15fb87a8f9c791e922b878861a1
Alexander Grishankov <grishankov.alexander@gmail.com>
2021-09-06 11:42:56 +0300

    Introduce function are_anagrams.

--- src/Chapter_01-Chapter_13/is_anagram/is_anagram.c
+++ src/Chapter_01-Chapter_13/is_anagram/is_anagram.c
@@ -9,15 +9,23 @@
 
 
 #define NUM_LETTERS 26
+#define STRING_SIZE 80
 #define INPUT_TERMINATOR '\n'
 
 
 void read_word(int counts[NUM_LETTERS]);
 bool equal_array(int counts1[NUM_LETTERS], int counts2[NUM_LETTERS]);
+// Returns true if s1 and s2 are anagrams
+bool are_anagrams(const char *s1, const char *s2);
+// Reads at most n - 1 characters into s. Excluding newline.
+int readln(char *s, int n);
 
 
 int main(void)
 {
+	/*
+	 * Worked all right but the exercise requires
+	 * function to compare 2 strings
 	bool anagrams;
 	int letter_count1[NUM_LETTERS] = {0};
 	int letter_count2[NUM_LETTERS] = {0};
@@ -30,6 +38,19 @@ int main(void)
 	anagrams = equal_array(letter_count1, letter_count2);
 
 	printf("Words are%s anagrams.\n", anagrams ? "" : " not");
+	*/
+
+	char s1[STRING_SIZE], s2[STRING_SIZE];
+
+	printf("Enter first word: ");
+	readln(s1, sizeof s1);
+	printf("Enter second word: ");
+	readln(s2, sizeof s2);
+
+	if (are_anagrams(s1, s2))
+		printf("Strings are anagrams\n");
+	else
+		printf("Not anagrams\n");
 
 	return 0;
 }
@@ -51,6 +72,47 @@ bool equal_array(int counts1[NUM_LETTERS], int counts2[NUM_LETTERS])
 
 	for (i = 0; i < NUM_LETTERS; i++)
 		if (counts1[i] != counts2[i])
+			return false;
+
+	return true;
+}
+
+
+int readln(char *s, int n)
+{
+	char c, *p;
+
+	for (p = s; n > 1; n--, p++) {
+		c = getchar();
+		if (c == '\n')
+			break;
+		*p = c;
+	}
+
+	*p = 0;
+
+	return p - s;
+}
+
+
+bool are_anagrams(const char *s1, const char *s2)
+{
+	int letter_count[NUM_LETTERS] = {0}, *p = letter_count;
+
+	while (*s1) {
+		if (isalpha(*s1))
+			letter_count[tolower(*s1) - 'a']++;
+		s1++;
+	}
+
+	while (*s2) {
+		if (isalpha(*s2))
+			letter_count[tolower(*s2) - 'a']--;
+		s2++;
+	}
+
+	for ( ; p < letter_count + NUM_LETTERS; p++)
+		if (*p)
 			return false;
 
 	return true;
