a3a6747723f7d7c6b52abfa68d319085e61aa2c9
Alexander Grishankov <grishankov.alexander@gmail.com>
2021-09-10 11:05:27 +0300

    Adds #ifdef, #elif, #else examples.

--- src/Chapter_14_The_Preprocessor/test_conditional_compilation/tcc.c
+++ src/Chapter_14_The_Preprocessor/test_conditional_compilation/tcc.c
@@ -15,6 +15,10 @@ int main(void)
 	{
 		PRINT_TOPIC("#if #endif");
 
+#if 1
+		printf("#if 1 - True\n");
+#endif
+
 #define DEBUG 1
 #if DEBUG
 		printf("DEBUG = %d\n", DEBUG);
@@ -44,6 +48,37 @@ int main(void)
 
 	{
 		PRINT_TOPIC("#ifdef and #ifndef");
+#ifndef DEBUG
+		printf("DEBUG not defined\n");
+#define DEBUG
+#ifdef DEBUG
+		printf("DEBUG defined\n");
+#undef DEBUG
+#endif /* #ifdef DEBUG */
+#endif /* #ifndef DEBUG */
+	}
+
+
+	{
+		PRINT_TOPIC("#elif and #else");
+
+#define DEBUG_MODE_3
+
+#ifdef NO_DEBUG
+#else
+	#if defined DEBUG_MODE_1
+		printf("DEBUG mode: 1\n");
+	#elif defined DEBUG_MODE_3
+		#undef DEBUG_MODE_3
+		#define DEBUG_MODE_2
+		printf("Disabled DEBUG mode: 3\n");
+		printf("Enabled DEBUG mode: 2\n");
+	#elif !defined DEBUG_MODE_2
+		#define DEBUG_MODE_2
+		printf("Enabled DEBUG mode: 2\n");
+	#endif /* #if defined DEBUG_MODE_1 */
+#endif /* #ifdef NO_DEBUG */
+
 	}
 
 
