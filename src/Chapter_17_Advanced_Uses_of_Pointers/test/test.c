b7352650af5f0ff7db0d907b0788509a91812c34
Alexander Grishankov <grishankov.alexander@gmail.com>
2021-09-25 18:08:17 +0300

    Creating strings on the heap.

--- src/Chapter_17_Advanced_Uses_of_Pointers/test/test.c
+++ src/Chapter_17_Advanced_Uses_of_Pointers/test/test.c
@@ -7,6 +7,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 
 // Prints topic name
@@ -15,17 +16,54 @@
 
 
 // Allocate memory for string argument
-static char *store_string(const char *str);
+static char *alloc_string(const char *str);
 // Concantenate s1 and s2 and allocate memory for them.
 static char *concat(const char *s1, const char *s2);
 
 
 int main(void)
 {
-	// Topic 1
+	// String allocation
 	{
-		PRINT_TOPIC(Topic, 1);
+		PRINT_TOPIC(String allocation);
+
+		char *s1 = alloc_string("Hello world!");
+		printf("s1: %s\n", s1);
+
+		char *s2 = concat("Hello", " world!");
+		printf("s2: %s\n", s2);
+
+		free(s1);
+		free(s2);
 	}
 
 	return 0;
+}
+
+
+static char *alloc_string(const char *str)
+{
+	char *p;
+
+	p = malloc(strlen(str) + 1);
+	if (!p)
+		printf("Allocation failed\n");
+	else
+		strcpy(p, str);
+
+	return p;
+}
+
+
+static char *concat(const char *s1, const char *s2)
+{
+	char *p;
+
+	p = malloc(strlen(s1) + strlen(s2) + 1);
+	if (!p)
+		printf("Allocation failed!\n");
+	else
+		strcpy(p, s1), strcat(p, s2);
+	
+	return p;
 }
