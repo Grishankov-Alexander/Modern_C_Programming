896855865bd72279a63c9f6a7b73d4e6c72c71f8
Alexander Grishankov <grishankov.alexander@gmail.com>
2021-10-02 12:50:09 +0300

    Adds newStr and newArray

--- src/Chapter_17_Advanced_Uses_of_Pointers/chapter_exercises/exercises.c
+++ src/Chapter_17_Advanced_Uses_of_Pointers/chapter_exercises/exercises.c
@@ -17,12 +17,88 @@
  * My wrapper around realloc.
  * If failure - free previously allocated memory and exit
  *
- * case 1: prevalloc 0, bytes 0 // dummy call - LET IT BE
- * case 2: prevalloc 1, bytes 0 // freeing - LET IT BE;
- * case 3: prevalloc 0, bytes 1 // malloc
- * case 4: prevalloc 1, bytes 1 // Shrink or grow
+ * case 1: prevalloc 0, bytes 0 // dummy call - LET IT BE. Returns not NULL
+ * case 2: prevalloc 1, bytes 0 // freeing - LET IT BE. Returns NULL;
+ * case 3: prevalloc 0, bytes 1 // allocate - exits on failure
+ * case 4: prevalloc 1, bytes 1 // reallocate - free prev and exit on failure
 */
-void *mymalloc(void *prevalloc, size_t bytes)
+void *myMalloc(void *prevalloc, size_t bytes);
+
+void testMyMalloc(void);
+
+// Allocate memory, copy str into it, return memory allocated.
+char *newStr(const char *str);
+
+// Allocate memory for the array that has nmemb members of size szmemb.
+// Use *init to initialize every member
+void *newArray(const void *init, size_t nmemb, size_t szmemb);
+
+
+
+
+int main(void)
+{
+	// My malloc wrapper
+	{
+		PRINT_TOPIC(malloc wrapper);
+
+		char *mystr;
+
+		mystr = myMalloc(NULL, 0); // Allocates dummy memory. mystr != NULL
+		mystr = myMalloc(mystr, 0); // Frees memory. Set mystr to NULL
+		mystr = myMalloc(NULL, 1024); // Usual malloc
+		mystr = myMalloc(mystr, 2048); // Reallocates
+		mystr = myMalloc(mystr, 0);
+		//testMyMalloc();
+
+		printf("mystr: %s", mystr = newStr("Hello world!\n"));
+		mystr = myMalloc(mystr, 0);
+	}
+
+
+	// Allocate and initialize array
+	{
+		PRINT_TOPIC(Allocate and initialize array);
+
+		size_t n = 3;
+
+		int *a = newArray(&(int){3}, n, sizeof *a);
+
+		for (int *p = a; p < a + n; p++)
+			printf(" %d", *p);
+		printf("\n");
+
+		myMalloc(a, 0);
+	}
+
+
+	{
+		struct point {
+			int x, y;
+		};
+
+		struct rectangle {
+			struct point upper_left, lower_right;
+		};
+
+		struct rectangle *p = myMalloc(NULL, sizeof *p);
+		p->upper_left = (struct point) {10, 25};
+		p->lower_right = (struct point) {20, 15};
+
+		myMalloc(p, 0);
+	}
+
+
+	{
+	}
+
+
+	return 0;
+}
+
+
+
+void *myMalloc(void *prevalloc, size_t bytes)
 {
 	void *new;
 
@@ -32,7 +108,7 @@ void *mymalloc(void *prevalloc, size_t bytes)
 		if (prevalloc)
 			free(prevalloc);
 		printf(	"\nAllocation failed. Exit status: FAILURE.\n"
-			"\tmymalloc(%p, %zu)\n", prevalloc, bytes);
+			"\tmyMalloc(%p, %zu)\n", prevalloc, bytes);
 		exit(EXIT_FAILURE);
 	}
 
@@ -40,27 +116,37 @@ void *mymalloc(void *prevalloc, size_t bytes)
 }
 
 
-int main(void)
+void testMyMalloc(void)
 {
-	// My malloc wrapper
-	{
-		PRINT_TOPIC(malloc wrapper);
-		char *mystr;
-		mystr = mymalloc(NULL, 0); // Allocates dummy memory. mystr != NULL
-		mystr = mymalloc(mystr, 0); // Frees memory. mystr == NULL
-		mystr = mymalloc(NULL, 1024); // Usual malloc
-		mystr = mymalloc(mystr, 2048); // Reallocates
-		printf("All OK\n");
+	char *mystr = NULL;
+	size_t sz = 1024UL;
 
-		size_t sz = 1024UL;
-		do {
-			printf("mymalloc(%p, %zu)\n", (void *) mystr, sz);
-			mystr = mymalloc(mystr, sz);
-			memset(mystr, 0xff, sz);
-			sz *= 16UL;
-		} while (mystr);
+	do {
+		printf("myMalloc(%p, %zu)\n", (void *) mystr, sz);
+		mystr = myMalloc(mystr, sz);
+		memset(mystr, 0xff, sz);
+		sz *= 16UL;
+	} while (mystr);
+}
+
+
+char *newStr(const char *str)
+{
+	return strcpy(myMalloc(NULL, strlen(str) + 1), str);
+}
+
+
+void *newArray(const void *init, size_t nmemb, size_t szmemb)
+{
+	void *a = myMalloc(NULL, nmemb * szmemb);
+
+	if (!init) {
+		memset(a, '\x0', nmemb * szmemb);
+		return a;
 	}
 
+	for (char *p = a; p < (char *) a + nmemb * szmemb; p += szmemb)
+		memcpy(p, init, szmemb);
 
-	return 0;
+	return a;
 }
