521637f0c4dc8fa3bba97a45409f6329f974e973
Alexander Grishankov <grishankov.alexander@gmail.com>
2021-11-16 18:45:47 +0300

    Formatted Input/Output, File Errors.

--- src/Chapter_22_Input_Output/test/test.c
+++ src/Chapter_22_Input_Output/test/test.c
@@ -33,9 +33,10 @@ int main(int argc, char **argv)
 
 
 SECOND_TOPIC:
+	/*
+	 * Temporary Files and Names
+	*/
 	{
-		PRINT_TOPIC(Temporary Files and Names);
-
 		FILE *tfp;
 		char filename[L_tmpnam];
 		char *filename2;
@@ -49,9 +50,10 @@ SECOND_TOPIC:
 	}
 
 
+	/*
+	 * File Buffering
+	*/
 	{
-		PRINT_TOPIC(File Buffering);
-
 		FILE *fp;
 		char *buf;
 		char *name;
@@ -78,9 +80,10 @@ SECOND_TOPIC:
 	}
 
 
+	/*
+	 * File Removal and Renaming
+	*/
 	{
-		PRINT_TOPIC(File Removal and Renaming);
-
 		if (remove("./buf"))
 			printf("Remove Failed\n");
 		if (rename("buffered", "../test/buf"))
@@ -89,8 +92,125 @@ SECOND_TOPIC:
 
 
 	{
-		PRINT_TOPIC(Attaching file to a stream);
+		PRINT_TOPIC(fprintf, fscanf);
 
+
+		FILE *fp;
+		const char *fn = "formatted.txt";
+
+		int wrote_total, read_total;
+		int wrote_so_far, read_so_far;
+		int n1, n2;
+		n1 = n2 = 0xABC;
+
+#		define FIELD_WIDTH		6
+#		define FIELD_PRECISION		4
+
+		{
+			fp = fopen(fn, "w");
+			wrote_total = fprintf(	// Return number of characters written
+				fp,
+				"%#*.*X"	// # - Begin hex numbers with 0X.
+						// * - Obtain Field Width and Precision from arguments
+				"%n"		// Store number of characters written so far in argument
+						// of type int *
+				"%*X"
+				"\n"
+				, FIELD_WIDTH, FIELD_PRECISION, n1
+				, & wrote_so_far
+				, FIELD_WIDTH, n2
+			);
+			fprintf(
+				fp,
+				"wrote_total: %d\n"
+				"wrote_so_far: %d\n"
+				, wrote_total, wrote_so_far
+			);
+			fclose(fp);
+		}
+
+		{
+			char skipped_string[FIELD_WIDTH + 1];
+
+			fp = fopen(fn, "r");
+			read_total = fscanf(	// Return number of stored scans
+				fp,
+				"%*X"		// * - Don't store this scan
+				"%[^\n]"	// Scan any character that doesn't belong to the set.
+						// (Scan until newline)
+				"%n"		// Store number of characters read so far
+				" %*13c"	// Skip whitespaces/newlines,
+						// scan 13 characters
+						// Don't store this scan
+				"%2d"		// Scan 2 digits
+				, skipped_string
+				, &read_so_far
+				, &wrote_total
+			);
+			fprintf(
+				stdout,
+				"read_total: %d\n"
+				"skipped_string: %s\n"
+				"read_so_far: %d\n"
+				"wrote_total: %d\n"
+				, read_total
+				, skipped_string
+				, read_so_far
+				, wrote_total
+			);
+			fclose(fp);
+		}
+	}
+
+
+	/*
+	 * File Errors
+	*/
+	{
+		/*
+		 * Search a file for a line that begins with an integer.
+		 *
+		 * Return integer on success
+		 * Return error code on FAILURE
+		*/
+		const char *fn = "formatted.txt";
+		FILE *fp;
+		int n;
+
+#		define END_OF_FILE	-1
+#		define FILE_ERROR	-2
+#		define NO_SUCH_FILE	-3
+
+
+		if (!(fp = fopen(fn, "r"))) {
+			n = NO_SUCH_FILE;
+			goto ROUTINE_END;
+		}
+
+		while (!(fscanf(fp, "%d", &n))) {
+			if (ferror(fp)) {
+				n = FILE_ERROR;
+				goto ROUTINE_END;
+			}
+			else if (feof(fp)) {
+				n = END_OF_FILE;
+				goto ROUTINE_END;
+			}
+			(void) (fscanf(fp, "%*[^\n]") + 1);
+		}
+
+		ROUTINE_END:
+		if (fp) {
+			clearerr(fp);	// Clear End-Of-File and File-Error indicators
+			fclose(fp);
+		}
+	}
+
+
+	/*
+	 * Attaching file to a stream
+	*/
+	{
 		if (freopen("stdoutfile", "w", stdout))
 			printf("Redirected stdout to stdoutfile\n");
 	}
